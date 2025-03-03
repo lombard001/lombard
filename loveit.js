ceacdb7edd416fa8b548b72e6fcece28764a39ee
lombard001 <sehan.beydullayev12@gmail.com>
2025-03-03 14:25:03 +0400

    Create loveit.js

---
+++ loveit.js
@@ -0,0 +1,4 @@
+const script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/gh/palaceofficial/MSP2cheats@refs/heads/main/Loveit.js";
+script.onload = () => {};
+document.head.appendChild(script);
