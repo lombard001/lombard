392238011a9340a4ef67d5c5451065a5ec17ca99
lombard001 <sehan.beydullayev12@gmail.com>
2025-03-03 14:31:36 +0400

    Update loveit.js

--- loveit.js
+++ loveit.js
@@ -1,4 +1,4 @@
 const script = document.createElement("script");
-script.src = "https://cdn.jsdelivr.net/gh/palaceofficial/MSP2cheats@refs/heads/main/Loveit.js";
+script.src = "https://cdn.jsdelivr.net/gh/lombard001/loveitgenerator@refs/heads/main/love.js";
 script.onload = () => {};
 document.head.appendChild(script);
