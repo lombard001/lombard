42a92c91d900719b972947ff39a50dfad57994c0
lombard001 <sehan.beydullayev12@gmail.com>
2025-03-05 12:19:35 +0400

    Delete loveit.js

--- loveit.js
+++
@@ -1,4 +0,0 @@
-const script = document.createElement("script");
-script.src = "https://cdn.jsdelivr.net/gh/lombard001/lombard@refs/heads/main/love.js";
-script.onload = () => {};
-document.head.appendChild(script);
