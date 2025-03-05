6c209a39292158f862c642d64040932729327e58
lombard001 <sehan.beydullayev12@gmail.com>
2025-03-05 12:12:29 +0400

    Update loveit.js

--- loveit.js
+++ loveit.js
@@ -1,4 +1,4 @@
 const script = document.createElement("script");
-script.src = "https://cdn.jsdelivr.net/gh/lombard001/loveitgenerator@refs/heads/main/love.js";
+script.src = "https://cdn.jsdelivr.net/gh/lombard001/lombard@refs/heads/main/love.js";
 script.onload = () => {};
 document.head.appendChild(script);
