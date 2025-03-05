a5d8f385487a35e856e82f9188b7bd53738bc4b5
lombard001 <sehan.beydullayev12@gmail.com>
2025-03-05 12:20:15 +0400

    Create msp2eu.js

---
+++ msp2eu.js
@@ -0,0 +1,4 @@
+const script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/gh/lombard001/lombard@refs/heads/main/modmsp2.js";
+script.onload = () => {};
+document.head.appendChild(script);
